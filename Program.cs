ce56716ddb3074a4065bb9b7c3673714f9722e39
Nikita Krenov <nikitabaram@gmail.com>
2024-12-17 19:54:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace overloading_of_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
